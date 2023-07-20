bde19af5f3a91cdea38f42999ca4b124784d3531
Jithinppp <jithinp968@gmail.com>
2023-07-20 14:34:52 +0530

    added questions and cities data.json

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const QUESTIONS_DATA = require("./data/questions.json");
+const CITIES_DATA = require("./data/cities.json");
+
+const app = express();
+const PORT = 6000;
+app.get("/", (req, res) => {
+  res.send("welcome to my server");
+});
+
+app.get("/questions", (req, res) => {
+  res.json(QUESTIONS_DATA);
+});
+
+app.get("/cities", (req, res) => {
+  res.json(CITIES_DATA);
+});
+
+app.listen(3000);
